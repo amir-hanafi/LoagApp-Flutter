75db4e85e14312c8e29738892f38ae6b85105c9e
amir-hanafi <amir.hanafi.s28@gmail.com>
2025-11-04 15:26:29 +0700

    11

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loagapps/pages/list_page.dart';
 import 'package:loagapps/pages/login_page.dart';
 import 'package:loagapps/pages/home_page.dart';
 
@@ -17,8 +18,57 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const LoginPage(),
-        '/home': (context) => const HomePage(),
+        '/home': (context) => const MainPage(),
       },
+    );
+  }
+}
+
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  // Daftar halaman
+  final List<Widget> _pages = const [
+    HomePage(),
+    ListPage()
+  ];
+
+  // Saat item navbar ditekan
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed, // agar 3+ item bisa tampil
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Barang',
+          ),
+        ],
+      ),
     );
   }
 }
